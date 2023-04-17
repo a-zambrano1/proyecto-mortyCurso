7c9cecfb82d270870a9abfc81328249e744bb3f8
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-04-17 13:30:19 -0500

    Anexos de pruebas

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App.js';
+import '@testing-library/jest-dom'
+import App from '../App.js';
 
 test('renders learn react link', () => {
-  render(App );
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
